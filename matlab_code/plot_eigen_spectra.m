2c75e4efdafdfa4aeb3287d953d6078d71594014
Jonathan Rainer <jonrainer@googlemail.com>
2016-02-10 11:21:06 +0000

    Added in a parameter to allow the colour to be passed into the plotting function which should allow us to differentiate which matrices are contributing eigenvalues and where.

--- matlab_code/plot_eigen_spectra.m
+++ matlab_code/plot_eigen_spectra.m
@@ -1,9 +1,9 @@
-function [] = plot_eigen_spectra(M)
+function [] = plot_eigen_spectra(M, colour_matrix)
 %PLOT_EIGEN_SPECTRA Summary of this function goes here
 %   Detailed explanation goes here
     eigenvalues = eig(M);
     % Plot Imag vs Real
-    plot(eigenvalues,'or')
+    plot(eigenvalues,'o', 'color', colour_matrix)
     axis([-plot_size plot_size -plot_size plot_size])
     grid
     set(gca,'xtick',-plot_size:plot_size,'ytick',-plot_size:plot_size);
