3d25353209246c44494548acc2edd3806cc8f7e9
Jonathan Rainer <jonrainer@googlemail.com>
2016-02-10 12:03:33 +0000

    Abstracted out generating the eigenvalues from generating the plots, should make question 2 easier.

---
+++ matlab_code/generate_eigenvalues.m
@@ -0,0 +1,11 @@
+function eigenvalues = generate_eigenvalues(iterations, matrix_size, ...
+    interaction_prob, d, mode )
+%GENERATE_EIGENVALUES Summary of this function goes here
+%   Detailed explanation goes here
+    eigenvalues = zeropls(matrix_size, iterations);
+    for i=1:iterations
+        M = generate_at_matrix(matrix_size, interaction_prob, d, mode);
+        eigenvalues(:,i) = eig(M);
+    end
+end
+
--- matlab_code/plot_eigen_spectra.m
+++ matlab_code/plot_eigen_spectra.m
@@ -1,12 +1,18 @@
-function [] = plot_eigen_spectra(M, colour_matrix)
+function [] = plot_eigen_spectra(eigenvalues)
 %PLOT_EIGEN_SPECTRA Summary of this function goes here
 %   Detailed explanation goes here
-    eigenvalues = eig(M);
-    % Plot Imag vs Real
-    plot(eigenvalues,'o', 'color', colour_matrix)
+    clf
+    hold on
+    for i=1:size(eigenvalues,2)
+        plot_size = round(sqrt(size(eigenvalues,1))) + 4;
+        % Plot Imag vs Real
+        plot(eigenvalues(:,i),'o', 'color', rand(1,3))
+    end
+    hold off
     axis([-plot_size plot_size -plot_size plot_size])
     grid
-    set(gca,'xtick',-plot_size:plot_size,'ytick',-plot_size:plot_size);
+    set(gca,'xtick',-plot_size:1:plot_size, ...
+        'ytick',-plot_size:1:plot_size);
     xlabel('Re');
     ylabel('Im');
     % Plot origin lines
