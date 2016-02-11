8c2bcbde5ccae9985116a92f6d7c7d95a3a628af
Jonathan Rainer <jonrainer@googlemail.com>
2016-02-11 16:48:35 +0000

    "Updated commentary ready for insertion into the report."

--- matlab_code/generate_eigenvalues.m
+++ matlab_code/generate_eigenvalues.m
@@ -1,8 +1,11 @@
 function eigenvalues = generate_eigenvalues(iterations, matrix_size, ...
     interaction_prob, d, mode, sigma)
-%GENERATE_EIGENVALUES Summary of this function goes here
-%   Detailed explanation goes here
+%GENERATE_EIGENVALUES Generate sets of eigenvalues to be plotted.
+    
+    % Create a structure to hold the eigenvalues generated.
     eigenvalues = zeros(matrix_size, iterations);
+    % Generate one matrix per iteration and extract it's eigenvalues into
+    % one column of the matrix defined.
     for i=1:iterations
         M = generate_at_matrix(matrix_size, interaction_prob, d, mode, ...
             sigma);
--- matlab_code/plot_eigen_spectra.m
+++ matlab_code/plot_eigen_spectra.m
@@ -1,21 +1,37 @@
-function [] = plot_eigen_spectra(eigenvalues)
-%PLOT_EIGEN_SPECTRA Summary of this function goes here
-%   Detailed explanation goes here
+function [] = plot_eigen_spectra(eigenvalues, colour)
+%PLOT_EIGEN_SPECTRA Given a vector of eigenvales, plot their spectra.
+
     hold on
+    % Calculate the size of the plot which seems to scale with the square
+    % of the size of the matrix the eigenvalues came from, with an
+    % additional factor so it doesn't look squashed on the page.
+    plot_size = round(sqrt(size(eigenvalues,1))) + 4;
+    % Iterate over the eigenvalues
     for i=1:size(eigenvalues,2)
-        plot_size = round(sqrt(size(eigenvalues,1))) + 4;
-        % Plot Imag vs Real
-        plot(eigenvalues(:,i),'o', 'color', rand(1,3))
+        % If the colour specifier is set then plot everything in one
+        % colour, otherwise choose one randomly per set of eigenvalues.
+        % This allows us to see how sets of eigenvalues fall or two
+        % independent sets look, depednent on context.
+        if ~strcmp('random', colour)
+            plot(eigenvalues(:,i),'o', 'color', colour)
+        else
+            plot(eigenvalues(:,i),'o', 'color', rand(1,3))
+        end
     end
     hold off
+    % Alter the axis so the whole diagram can be seen, add a grid and make
+    % sure the axis are equally spaced in the digram.
     axis([-plot_size plot_size -plot_size plot_size])
     grid
     axis equal
-    set(gca,'xtick',-plot_size:1:plot_size, ...
-        'ytick',-plot_size:1:plot_size);
+    % Add in markers to the scales so it's obvious how large or small the
+    % generate spectra are
+    set(gca,'xtick',-plot_size:10:plot_size, ...
+        'ytick',-plot_size:10:plot_size);
+    % Label the real and imaginary axis.
     xlabel('Re');
     ylabel('Im');
-    % Plot origin lines
+    % Plot axis lines
     hold on
     plot([0 0],[-plot_size plot_size],'k');
     plot([-plot_size plot_size],[0 0],'k');
