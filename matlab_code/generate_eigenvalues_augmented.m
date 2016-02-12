7d5b7823a0a9a2f845976813cf1e7412cef9a82c
Jonathan Rainer <jonrainer@googlemail.com>
2016-02-12 07:53:40 +0000

    "Updated how the function works to return the augemented and un-augmented eigenvalues for comparison purposes."

--- matlab_code/generate_eigenvalues_augmented.m
+++ matlab_code/generate_eigenvalues_augmented.m
@@ -1,13 +1,15 @@
-function eigenvalues = generate_eigenvalues_augmented(iterations, ...
-    matrix_size, interaction_prob, d, mode, sigma)
+function [eigenvalues, eigenvalues_a] = generate_eigenvalues_augmented(...
+    iterations, matrix_size, interaction_prob, d, mode, sigma)
 %GENERATE_EIGENVALUES_AUGMENTED Summary of this function goes here
 %   Detailed explanation goes here
     eigenvalues = zeros(matrix_size, iterations);
+    eigenvalues_a = zeros(matrix_size, iterations);
     for i=1:iterations
         M = generate_at_matrix(matrix_size, interaction_prob, d, mode, sigma);
         scalars = repmat((2*(rand(matrix_size, 1))), 1, matrix_size);
         M_t = scalars.*M;
-        eigenvalues(:,i) = eig(M_t);
+        eigenvalues(:,i) = eig(M);
+        eigenvalues_a(:,i) = eig(M_t);
     end
 end
 
